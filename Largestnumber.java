59361bf8361aae05bfeab448f7ec00df4acfd51a
mandviyadav <mandvi6098@gmail.com>
2026-02-02 11:21:13 +0530

    Create Largestnumber.java

---
+++ Largestnumber.java
@@ -0,0 +1,15 @@
+public class largestnumber{
+  public static void main(String[] args) {
+    int a=10, b=20, c=35;
+    if(a > b  && a > c) {
+      System.out.println(a);
+    } else if (b > c) {
+      system.out.println(b);
+    } else {
+      system.out.println(c);
+    }
+  }
+}
+    
+       
+  
