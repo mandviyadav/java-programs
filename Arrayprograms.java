8f2caaaecc6a34b7f2b546edc7023d57ff84c5e0
mandviyadav <mandvi6098@gmail.com>
2026-02-02 21:18:14 +0530

    Create Arrayprograms.java

---
+++ Arrayprograms.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayEasy {
+  static void printArray(int[]a){
+    for (int x: a)
+  System.out.println(x + "");
+    System .out.println();
+  }
+ 
+  
+  
+  
+    
+    
